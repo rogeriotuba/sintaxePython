d3f8151ba963e29c3628bce9eca9bb36d8807164
rogerioadm <rsantosandrade7@gmail.com>
2021-08-03 19:14:33 -0300

    Sintaxe Python 01

---
+++ aulasPythonSintaxe/comandosBasicos.py
@@ -0,0 +1,15 @@
+
+num = 10;
+frase = "Ola Mundo ";
+
+print(frase + str(num) + "!" );
+
+
+escolha = True;
+decimal = 0.10;
+
+print(escolha);
+print(decimal);
+
+#print(frase +str(num)+str(escolha)+str(decimal));
+print(str( num + decimal));
